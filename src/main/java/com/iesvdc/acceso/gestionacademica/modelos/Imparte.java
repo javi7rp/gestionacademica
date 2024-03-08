f91c2190a291c8502faeb67252453d2ace915237
manana <jredondop@outlook.es>
2024-03-08 12:15:56 +0100

    añadido modelos para matricular e impartir

---
+++ src/main/java/com/iesvdc/acceso/gestionacademica/modelos/Imparte.java
@@ -0,0 +1,11 @@
+package com.iesvdc.acceso.gestionacademica.modelos;
+
+import lombok.Data;
+
+@Data
+public class Imparte {
+
+    private Long profesorId;
+    private Long asignaturaId;
+    
+}
---
+++ src/main/java/com/iesvdc/acceso/gestionacademica/modelos/Matricula.java
@@ -0,0 +1,11 @@
+package com.iesvdc.acceso.gestionacademica.modelos;
+
+import lombok.Data;
+
+@Data
+public class Matricula {
+
+    private Long alumnoId;
+    private Long asignaturaId;
+    
+}
